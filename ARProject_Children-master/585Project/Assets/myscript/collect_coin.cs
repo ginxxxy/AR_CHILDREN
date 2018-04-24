93411100841af4253ab028d55d60bf5183ad4bf2
mailaw <mlaw534@gmail.com>
2018-04-24 00:43:44 -0400

    first commit

---
+++ ARProject_Children-master/585Project/Assets/myscript/collect_coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collect_coin : MonoBehaviour {
+	public GameObject coins;
+	public GameObject coins2;
+	public GameObject coins3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void onClick(){
+		if (coins.activeSelf == true) {
+			Destroy (coins);
+			Scorecount.scorevalue++;
+		}
+		if (coins2.activeSelf == true) {
+			Destroy (coins2);
+			Scorecount.scorevalue++;
+		}
+		if (coins3.activeSelf == true) {
+			Destroy (coins3);
+			Scorecount.scorevalue++;
+		}
+	}
+}
